44101b4663c4224abaedeb763f7e4f44d9477ceb
Oliver Strobel <oliver.strobel@venturus.org.br>
2017-12-14 09:42:37 -0200

    Ajustes

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthorizationService } from '../services/authorization.service';
 import { ConnectionService } from '../services/connection.service';
-import { Media } from '../models/media';
 import { Router } from '@angular/router';
 
 @Component({
@@ -16,11 +15,15 @@ export class MainComponent implements OnInit {
   private _mediaIndex = 0;
   public inAnimation = true;
   public outAnimation = false;
-  public selectedMedia: Media;
+  public selectedMedia;
   public tag = 'venturus';
+  public durationSeconds = 5;
+  public refreshMinutes = 5;
 
   ngOnInit() {
-    this.getMediaItems('venturus');
+    this.getMediaItems(this.tag);
+    setInterval(() => { this.getMediaItems(this.tag); }, ( this.refreshMinutes * 60000 ));
+    setInterval(this.switchMedia.bind(this), ( this.durationSeconds * 1000 ));
   }
 
   checkAuthorization() {
@@ -28,36 +31,34 @@ export class MainComponent implements OnInit {
   }
 
   getMediaItems(tag: string): void {
+    console.log(`Searching for posts with #${tag}..`);
     this._connection.getTagMedia(tag).subscribe(
       data => {
+      console.log(data.length + ' posts ready...');
       this._mediaItems = data;
-      console.log(this._mediaItems);
-      this.selectedMedia = this._mediaItems[0];
-      this.switchMedia();
+      this._mediaIndex = -1;
     }, err => console.log(err));
   }
 
-  getLocationItems(): void {
-    this._connection.getLocationMedia(ConnectionService.LOCATIONS.venturus).subscribe(
+  getLocationItems(location: string): void {
+    console.log(`Searching for posts with location ${location}..`);
+    this._connection.getLocationMedia(location).subscribe(
       data => {
-      this._mediaItems = data;
-      this.selectedMedia = this._mediaItems[0];
-      this.switchMedia();
-    }, err => console.log(err));
+        console.log(data.length + ' posts ready...');
+        this._mediaItems = data;
+        this._mediaIndex = -1;
+      }, err => console.log(err));
   }
 
   switchMedia(): void {
-    setTimeout(() => {
       this.animate('out');
-      setTimeout(_ => {
+      setTimeout(() => {
         this._mediaIndex = this._mediaIndex === (this._mediaItems.length - 1) ? 0 : this._mediaIndex + 1;
         this.selectedMedia = this._mediaItems[this._mediaIndex];
       }, 500); // Wait for out transition complete to load other image
-      setTimeout(_ => {
+      setTimeout(() => {
         this.animate('in');
-      }, 600); // Wait for in transition complete to load other image
-      this.switchMedia();
-    }, 5000); // Wait time between one image and the other
+      }, 500); // Wait for in transition complete to load other image
   }
 
   private animate(direction: string) {
